5a8f32ed8a9c75326dacdf7be12f6ce11bd27190
Paul Nhan Nguyen Dao <daonhan@gmail.com>
2026-06-13 15:05:54 +0700

    Add: top-k-elements-in-list - submission-6

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-6.cs
@@ -0,0 +1,26 @@
+public class Solution {
+    public int[] TopKFrequent(int[] nums, int k) 
+    {
+        var map = new Dictionary<int, int>();
+		foreach (var n in nums)
+			map[n] = map.GetValueOrDefault(n) + 1;
+		
+        return map.OrderByDescending(m => m.Value).Take(k).Select(x => x.Key).ToArray();
+			
+		// var pq = new PriorityQueue<int, int>();
+		// foreach (var item in map)
+		// {
+		// 	pq.Enqueue(item.Key, item.Value);
+		// 	if (pq.Count > k) 
+		// 	{
+		// 		pq.Dequeue();			
+		// 	}
+		// }
+		// var results = new int[k];
+		// for (int i = 0; i < k; i++)
+		// {
+		// 	results[i] = pq.Dequeue();
+		// }
+		// return results;
+    }
+}
