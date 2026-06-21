f2f1deb6e16ccab53c05b738c5378d7d39abca75
Paul Nhan Nguyen Dao <daonhan@gmail.com>
2026-06-21 15:33:20 +0700

    Add: top-k-elements-in-list - submission-13

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-13.cs
@@ -0,0 +1,37 @@
+public class Solution 
+{
+    public int[] TopKFrequent(int[] nums, int k) 
+	{
+        var map = new Dictionary<int, int>();
+		foreach (var num in nums)
+		{
+			map[num] = map.GetValueOrDefault(num) + 1;
+		}
+		
+		List<int>[] freq = new System.Collections.Generic.List<int>[nums.Length +1];
+		
+		foreach (var kpv in map)
+		{
+			if (freq[kpv.Value] == null) 
+			{
+				freq[kpv.Value] = new List<int>();
+			}
+			freq[kpv.Value].Add(kpv.Key);
+		}
+		
+		int index =0;
+		int[] result = new int[k];
+		for (int i = freq.Length -1; i >0 && index < k ; i--)
+		{
+			if(freq[i] == null) continue;
+			foreach (var num in freq[i])
+			{
+				result[index++] = num;
+				if(index == k)
+					return result;
+			}
+		}
+		
+		return result;
+    }
+}
